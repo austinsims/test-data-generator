0ca4862ba5a2d9e180a2f9b9c8a6cf31bc1852cb
Austin Sims <adsims2001@gmail.com>
2014-11-17 00:16:17 -0500

    use knockout.js for data-binding.  rewrite formatting function using functional programming from underscore.js

--- index.js
+++ index.js
@@ -1,79 +1,83 @@
+function format(value) {
+    function isDate(val) {
+        var d = new Date(val);
+        return !isNaN(d.valueOf());
+    }
+    
+    if (value == '') return 'null';
 
-    $(document).ready(function() {
+    // No special formatting for boolean literal values or numbers.
+    else if (value == "null" || value == "true" || value == "false" || !isNaN(Number(value))) return value;
 
-        function format(data) {
-                function isDate(val) {
-                    var d = new Date(val);
-                    return !isNaN(d.valueOf());
-                }
+    // Leave already-quoted strings alone.
+    else if ( 
+           (value[0] == "\"" && value[value.length-1] == "\"") 
+        || (value[0] == "'" && value[value.length-1] == "'") 
+    ) return value;
 
-                // No special formatting for JavaScript keywords
-                if (data == "null" || data == "true" || data == "false" || !isNaN(Number(data))) return data;
+    // Try to detect dates and write them as C# conversion statements
+    else if (isDate(value)) return 'Convert.ToDateTime("' + value + '")';
 
-                // Leave quoted strings alone
-                else if ( (data[0] == "\"" && data[data.length-1] == "\"") || (data[0] == "'" && data[data.length-1] == "'") ) return data;
+    // If none of the above special cases are met... just wrap it
+    // up in quotes, escpae backslashes and hope it's supposed to be a string
+    else return '"' + value.replace(/\\/, '\\\\') + '"';
+}
 
-                // Try to detect dates and write them as C# conversion statements
-                else if (isDate(data)) return 'Convert.ToDateTime("' + data + '")';
+function TestDataGenerator() {
+    var self = this;
 
-                // Just wrap it up in quotes, escpae backslashes and hope it's supposed to be a string
-                else return '"' + data.replace(/\\/, '\\\\') + '"';
-
+    self.className = ko.observable('');
+    self.arrVarName = ko.observable();
+    self.indent = ko.observable(0);
+    self.declaration = ko.observable('private readonly field');
+    self.delim = ko.observable(',');
+    
+    self.input = ko.observable('');
+    
+    self.output = ko.computed(function() {
+        if (!self.className()) {
+            return 'Please fill out class name above.';
         }
+        
+        var lines = self.input().split('\n');
+        var header = lines[0].split(self.delim());
+        var body = lines.slice(1);
 
-            function handle(e) {
-                    var delim = $("#delim").val();
-                    var className = $("#className").val();
-                    var arrVarName = $("#arrVarName").val() || ('sample' + className + 's');
-                    var indentNum = Number($("#indent").val()) || 0;
-                    var indent = '';
-                    for (var i=0; i<indentNum; i++) indent += ' ';
+        var objectList = _.chain(body)
+            // Do not try to make objects out of empty lines.
+            .filter(function(line) {
+                return line != '';
+            })
+            // Split by the specified delimiter into an array of property values
+            .map(function(line) {
+                return line.split(self.delim());
+            })
+            .map(function(valueList) {
+                // Change each prop. value into 'propName = value,'
+                return _.map(valueList, function(value, index) {
+                        if (header[index]) return '        ' + header[index] + ' = ' + format(value) + ',';
+                    })
+                    .join('\n') + '\n';
+            })
+            // Add the new keyword, class name and braces to each list of keys and values
+            .map(function(objectBody) {
+                return '    new ' + self.className() + '() {\n' + objectBody + '    },';
+            })
+            .value()
+            .join('\n');
 
-                    var output = '';
-
-                    if (!className)
-                            output = 'Please fill out class name above'
-                    else {
-                            var declaration = null;
-                            var radioValue = $("input:radio[name='declaration']:checked").val();
-                            if (radioValue == 'var') declaration = 'var';
-                            else if (radioValue == 'field') declaration = 'private readonly ' + className + '[]';
-
-                            var arrayIndices = $("input:checkbox[name='arrayIndices']").is(':checked');
-
-                            var lines = $('textarea.input').val().split('\n');
-                            var header = null;
-
-                            output += indent +  declaration + ' ' + arrVarName + ' = new[] {\n';
-
-                            for (var i = 0; i < lines.length; i++) {
-                                    if (i == 0)
-                                            header = lines[i].split(delim);
-                                    else {
-                                            var cells = lines[i].split(delim);
-                                            if (cells.length == 1 && cells[0] == '') continue;
-                                            if (i > 1)
-                                                    output += ',\n\n';
-
-                                        if (arrayIndices)
-                                            output += indent +  '   ' +
-                                                ' // ' + String(i-1) + '\n';
-
-                                            output += indent +  '    new ' + className + '() {\n';
-                                            for (var j=0; j < header.length; j++) {
-                                                    var value = (typeof cells[j] == 'undefined' || cells[j] == '' ? 'null' : format(cells[j]));
-                                                    output += indent +  '        ' + header[j].trim() + ' = ' + value.trim() + (j == header.length-1 ? '' : ',') + '\n';
-                                            }
-                                            output += indent +  '    }';
-                                    }
-                            }
-
-                            output += '\n' + indent + '};\n';
-                    }
-
-                    $('textarea.output').val(output);
-            }
-
-            $('textarea, input, :radio, select').on('input propertychange click', handle);
-            $('textarea.output').on('click', function(e) { $(e.target).select(); });
+        // Add the type declaration, assignment operator, braces, etc.
+        var testData = self.declaration() + ' '
+            + (self.arrVarName() || 'sample' + self.className() + 's' ) + ' = new[] {\n'
+            + objectList + '\n'
+            + '};';
+        
+        // Prepend each line with the specified number of spaces.
+        return _.map(testData.split('\n'), function(line) {
+                return Array(Number(self.indent()) + 1).join(' ') + line; 
+            }).join('\n');
+        
     });
+}
+
+ko.applyBindings(new TestDataGenerator());
