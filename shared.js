84627515454b01a216ab16c1988f807b5224dfc2
Austin Sims <adsims2001@gmail.com>
2014-12-15 21:46:47 -0500

    add ko binding for help box. still needs twerk

--- shared.js
+++ shared.js
@@ -1,3 +1,24 @@
+ko.bindingHandlers.hidable = {
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var e = $(element);
+        e.css('position', 'relative');
+        closeButton = jQuery('<i class="fa fa-close" style="cursor: pointer; position: absolute; right: 5px; top: 5px"></i>');
+        e.append(closeButton);
+        e.wrap("<div id='wrapper'></div>");
+        uncloseButton = jQuery('<i class="fa fa-question" style="position: absolute; right: 5px; top: 5px; cursor: pointer; display: none"></i>');
+        $('#wrapper').append(uncloseButton);
+        closeButton.click(function() {
+            e.css('display', 'none');
+            uncloseButton.css('display', 'block');
+        });
+        uncloseButton.click(function() {
+            uncloseButton.css('display', 'none');
+            e.css('display', 'block');
+            closeButton.css('display', 'absolute');
+        });
+    }
+}
+
 function Base() {
   var self = this;
 
